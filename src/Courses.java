ab01aa7b41e25b8d6bef14895a1d247f29ed1e3d
BruhMomentos <55457974+BruhMomentos@users.noreply.github.com>
2020-02-10 14:59:23 -0800

    sd

---
+++ src/Courses.java
@@ -0,0 +1,20 @@
+public class Courses {
+    private String math;
+    private String chemistry;
+    private String marketing;
+    private String Fitness;
+    private int grade;
+
+    //Constructor
+
+    Courses(String math, String chemistry, String marketing, String fitness, int grade){
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/Students.java
@@ -0,0 +1,45 @@
+public class Students {
+    private String firstname;
+    private String lastname;
+    private int id;
+    private int[] courses;
+
+
+
+    //Constructor
+    Students(int id, String firstname, String lastname, int[] courses){
+        System.out.print("Student Id Card");
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.courses = courses;
+
+    }
+    //Methods
+    public int getid(){ return id; }
+
+    public String getfirstnamename(){
+        return firstname;
+    }
+
+    public String getlastname(){
+        return lastname;
+    }
+
+    public void setCourses(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
